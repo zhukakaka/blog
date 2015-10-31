3c6da6b844578b01c129b08657c04e548116395f
vagrant <vagrant@homestead>
2015-10-31 16:41:14 +0000

    a

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -1,47 +1,17 @@
 <?php
 
 return array(
-
-	/*
-	|--------------------------------------------------------------------------
-	| Database Connections
-	|--------------------------------------------------------------------------
-	|
-	| Here are each of the database connections setup for your application.
-	| Of course, examples of configuring each database platform that is
-	| supported by Laravel is shown below to make development simple.
-	|
-	|
-	| All database work in Laravel is done through the PHP PDO facilities
-	| so make sure you have the driver for your particular database of
-	| choice installed on your machine before you begin development.
-	|
-	*/
-
-	'connections' => array(
-
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-	),
+    'connections' => array(
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => isset($_ENV['MYSQL_DB_HOST']) ? $_ENV['MYSQL_DB_HOST'] : '192.168.1.200',
+            'database'  => isset($_ENV['MYSQL_DB_NAME']) ? $_ENV['MYSQL_DB_NAME'] : 'ydf',
+            'username'  => isset($_ENV['MYSQL_DB_USER']) ? $_ENV['MYSQL_DB_USER'] : 'ydf',
+            'password'  => isset($_ENV['MYSQL_DB_PWD']) ? $_ENV['MYSQL_DB_PWD'] : '111111',
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+        ),
+    ),
 
 );
--- app/models/User.php
+++ app/models/User.php
@@ -1,26 +1,12 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Auth\UserTrait;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface {
+    use UserTrait;
 
-	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
-
+    protected $table = 'users';
+    protected $hidden = array('password', 'remember_token');
+    protected $guard = array('email', 'password');
 }
